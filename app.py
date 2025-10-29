12864a7040ad0e22fa54a104c5c3daa9980efe50
nurulaina02 <ainasaharani24@gmail.com>
2025-10-29 19:20:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,19 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
 
-# --- Corrected Imports ---
-import plotly.graph_objects as go # Keep this if you need go, though px handles everything here
-
-# Set Streamlit page configuration (must be the first Streamlit command)
 st.set_page_config(
-    page_title="Mental Health Classification Analysis",
-    layout="wide" # Set layout here for consistency
+    page_title="Mental Health Classification Analysis"
 )
-visualise = st.Page('mentalhealth.py', title='Mental Health', icon=":material/medicition:")
+visualise = st.Page('mentalhealth.py', title='Mental Health Classification Analysis', icon=":material/school:")
 
-home = st.Page('app.py', title='Homepage', default=True, icon=":material/home:")
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
             "Menu": [home, visualise]
         }
-    )
+    )
 
 pg.run()
-# Page header
-st.header("Mental Health Classification Analysis", divider="grey")
-
-col1, col2, col3, col4 = st.columns(4)
-    
-col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
-col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
-col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
 
 # Load your data
 try:
